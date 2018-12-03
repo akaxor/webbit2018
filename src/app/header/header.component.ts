0420ad93428b60c57c8af341c52f61df2396d75b
akaxor <joakimbyvik@gmail.com>
2018-12-03 17:51:47 +0100

    updatingLots

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AppService} from '../app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private appService: AppService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  logout(){
+    alert("yolo");
+    this.router.navigate(['/api/logout']);
+  }
 }
